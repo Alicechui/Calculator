1ae6c58202dfbb537e7e115ef33ad24d1ffa6e4d
AliceChui <alicechuihy@outlook.com>
2023-10-23 14:25:41 +0200

    object created

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,11 +2,29 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator {
+public class Calculator implements ActionListener{
+	JFrame frame;
+	JTextField textfield;
+	JButton[] numberButtons = new JButton[10];
+	JButton[] functButtons = new JButton[8];
+	JButton addBut, subBut, mulBut, divBut, deciBut, eqlBut, clearBut;
+	JPanel panel;
+	
+	Font myFont = new Font("",Font.BOLD,30);
+	
+	Calculator(){
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Calculator calc = new Calculator();
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
